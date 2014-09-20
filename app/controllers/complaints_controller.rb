842ed1e9b2735901432b13302cb52b9599a24e51
Igor Krupa <igor.krupa@gram.pl>
2014-09-20 12:46:29 +0200

    complaint controller

---
+++ app/controllers/complaints_controller.rb
@@ -0,0 +1,30 @@
+class ComplaintsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+
+	expose(:complaints)
+
+	def index		
+	end
+
+	def new		
+	end
+
+	def show		
+	end
+
+	def create
+		self.complaint = Complaint.new(complaint_params)
+
+		if complaint.save
+			redirect_to complaint, notice: 'Thank you for your complaint'
+		else
+			render action: 'new'
+		end
+	end
+	
+	private
+
+	def complaint_params
+		params.require(:complaint).permit(:name, :content, :url)
+	end
+end
