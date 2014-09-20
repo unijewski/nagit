8c3c0458fe8fddff976fc9ea7791826806944ca8
Grzegorz Suder <gregor.afc@gmail.com>
2014-09-20 13:44:56 +0200

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,14 @@ ActiveRecord::Schema.define(version: 20140920102408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "complaints", force: true do |t|
+    t.string   "name"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "loves", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
