f7b520442020343e91622fb33c5cdb55d2ce33b4
Grzegorz Suder <gregor.afc@gmail.com>
2014-09-20 12:04:35 +0200

    Seeds for Negger

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Nagger.delete_all
+
+(1..5).each do |number|
+  Nagger.create( email: "joe.doe#{number}@example.com", password: "test1234", password_confirmation: "test1234")
+end
