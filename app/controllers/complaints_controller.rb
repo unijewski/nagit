e825ac744836ff125e5284342e2232f3dc12b909
Dodo <dodox360@gmail.com>
2014-09-20 13:58:50 +0200

    Love beta

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,7 +1,8 @@
 class ComplaintsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
-	expose(:complaints)
+	#expose(:complaints)
+	#expose(:love)
 
 	def index		
 	end
@@ -21,9 +22,23 @@ class ComplaintsController < ApplicationController
 			render action: 'new'
 		end
 	end
+	def love
+    unless @complaint.user == current_user
+      Love.user = current_user
+      if Love.save
+        redirect_to complaint_path(complaint)
+        flash[:notice] = "You love it!"
+      else
+      	redirect_to complaint_path(complaint)
+      end
+    else
+      flash[:error] = "You can't love yourself!"
+    end
+  end
+
 	
 	private
-
+	
 	def complaint_params
 		params.require(:complaint).permit(:name, :content, :url)
 	end
