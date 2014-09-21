c3468da7a180caac2a836a46e2eae46ae85f94b3
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-09-21 11:17:03 +0200

    Top complaints

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -2,4 +2,8 @@ module ComplaintsHelper
   def top5_naggers
     Nagger.all.order('complaints_count desc').take(5)
   end
+
+  def top5_complaints
+    Complaint.all.order('loves_count desc').take(5)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921084159) do
+ActiveRecord::Schema.define(version: 20140921091139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140921084159) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "nagger_id"
+    t.integer  "loves_count", default: 0, null: false
   end
 
   create_table "loves", force: true do |t|
