8baf9dc60837c92293bee2d448e263cf69a18b56
Igor Krupa <igor.krupa@gram.pl>
2014-09-21 16:29:28 +0200

    api create complaint

--- app/controllers/api/complaints_controller.rb
+++ app/controllers/api/complaints_controller.rb
@@ -1,13 +1,33 @@
 class Api::ComplaintsController < ApplicationController
 	expose(:complaints)
-	expose(:complaint)
+	expose(:complaint, attributes: :complaint_params)
 
 	def index
 		render json: complaints
 	end
 
+	def new
+		
+	end
+
 	def show
 		render json: complaint
 	end
+
+	def create
+		#self.complaint = Complaint.new
+		binding.pry
+
+		#complaint.nagger_id = complaint.nagger_id
+		if complaint.save
+			render nothing: true, status: 201
+		else
+			render json: complaint.errors.to_json, status: 422
+		end
+	end
+
+	def complaint_params
+    	params.require(:complaint).permit(:name, :content, :nagger_id)
+  	end
 
 end
